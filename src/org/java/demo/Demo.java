4ada9c75804e779138f91435654287337045e40c
root <1990831039@qq.com>
2019-12-05 09:29:45 +0800

    第一次

---
+++ src/org/java/demo/Demo.java
@@ -0,0 +1,14 @@
+package org.java.demo;
+
+/**
+ * @Author:蔡吉东
+ * @Date:2019/12/5 09:28
+ * @Description:
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("22222222222222222222222222222222222222222");
+    }
+
+}
